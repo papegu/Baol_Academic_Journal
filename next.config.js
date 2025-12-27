6ebf156d1f6e7a875063688d398573e55e50de67
Pape Elhadj Abdoulaye Gueye <papeabdoulaye.gueye@uadb.edu.sn>
2025-12-27 06:11:20 +0100

    Add Prisma datasource url; verify Tailwind/Next configs; hyphenated brand utilities across app; Windows dev stability tweaks

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     // Reduce install/build noise on Vercel; still lint locally
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { dev }) => {
+    // Workaround for Windows UNKNOWN/EPERM errors on filesystem cache/renames
+    // Disable Webpack persistent filesystem cache in dev
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
