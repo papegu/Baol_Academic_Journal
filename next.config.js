6b0a7d692f54d21b5003cfb220b3b2b10d3ddc9e
Pape Elhadj Abdoulaye Gueye <papeabdoulaye.gueye@uadb.edu.sn>
2025-12-27 01:35:10 +0100

    chore: ignore ESLint during builds to reduce Vercel deprecation warnings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Reduce install/build noise on Vercel; still lint locally
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
